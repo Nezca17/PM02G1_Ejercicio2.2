e57e92aee8fc018f20d3d1092176559ccc96ba0b
Nezca17 <126973834+Nezca17@users.noreply.github.com>
2023-07-19 23:26:21 -0600

    prueabas

--- PM02G1_Ejercicio2.2/PM02G1_Ejercicio2.2/MainPage.xaml.cs
+++ PM02G1_Ejercicio2.2/PM02G1_Ejercicio2.2/MainPage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using Xamarin.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+using PM02G1_Ejercicio2._2.Models;
+using System.Collections.Generic;
 
 namespace PM02G1_Ejercicio2._2
 {
@@ -9,12 +14,15 @@ namespace PM02G1_Ejercicio2._2
         // Colección para almacenar los elementos del ListView
         ObservableCollection<string> itemsCollection = new ObservableCollection<string>();
 
+        private ListaApi datos;
+
         public MainPage()
         {
             InitializeComponent();
 
             // Asignar la colección al ListView
             itemsListView.ItemsSource = itemsCollection;
+            datos = new ListaApi();
         }
 
         // Evento cuando se presiona el botón para agregar un nuevo elemento
@@ -23,10 +31,44 @@ namespace PM02G1_Ejercicio2._2
             string newItem = newItemEntry.Text;
             if (!string.IsNullOrWhiteSpace(newItem))
             {
-                // Agregar el nuevo elemento a la colección y limpiar el Entry
+               // Agregar el nuevo elemento a la colección y limpiar el Entry
                 itemsCollection.Add(newItem);
                 newItemEntry.Text = string.Empty;
             }
         }
+
+        private void CargarApi(object sender, EventArgs e)
+        {
+            Solicitud();
+        }
+
+
+        public async void Solicitud() {
+
+            var requet = new HttpRequestMessage();
+            requet.RequestUri = new Uri("https://jsonplaceholder.typicode.com/posts");
+            requet.Method = HttpMethod.Get;
+
+            requet.Headers.Add("Accpet", "application/json");
+
+            var client = new HttpClient();
+            //respuesta de la solicitud
+            HttpResponseMessage response = await client.SendAsync(requet);
+
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+
+                // convertir el Json a String, osea deserealizarlo
+                var resultado = JsonConvert.DeserializeObject<List<ListaApi>>(content);
+                //Cargarle los datos al listview
+                itemsListView.ItemsSource = resultado;
+
+
+            }
+
+        
+        }
+
     }
 }
---
+++ PM02G1_Ejercicio2.2/PM02G1_Ejercicio2.2/Models/ListaApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace PM02G1_Ejercicio2._2.Models
+{
+            public class ListaApi
+        {
+                    [JsonProperty("userId")]
+                public long UserId { get; set; }
+
+                [JsonProperty("id")]
+                public long Id { get; set; }
+
+                [JsonProperty("title")]
+                public string Title { get; set; }
+
+                [JsonProperty("body")]
+                public string Body { get; set; }
+        }
+}

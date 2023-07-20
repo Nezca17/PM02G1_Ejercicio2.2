42e874bee7ac4257a644afe6417ee83d1fc3079c
gereld10 <gereldamids42@gmail.com>
2023-07-19 22:45:20 -0600

    Agregando el ListView

--- PM02G1_Ejercicio2.2/PM02G1_Ejercicio2.2/MainPage.xaml.cs
+++ PM02G1_Ejercicio2.2/PM02G1_Ejercicio2.2/MainPage.xaml.cs
@@ -1,18 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace PM02G1_Ejercicio2._2
 {
     public partial class MainPage : ContentPage
     {
+        // Colección para almacenar los elementos del ListView
+        ObservableCollection<string> itemsCollection = new ObservableCollection<string>();
+
         public MainPage()
         {
             InitializeComponent();
+
+            // Asignar la colección al ListView
+            itemsListView.ItemsSource = itemsCollection;
+        }
+
+        // Evento cuando se presiona el botón para agregar un nuevo elemento
+        private void OnAddItemButtonClicked(object sender, EventArgs e)
+        {
+            string newItem = newItemEntry.Text;
+            if (!string.IsNullOrWhiteSpace(newItem))
+            {
+                // Agregar el nuevo elemento a la colección y limpiar el Entry
+                itemsCollection.Add(newItem);
+                newItemEntry.Text = string.Empty;
+            }
         }
     }
 }
